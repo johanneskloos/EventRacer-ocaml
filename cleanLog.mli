50d948146f4877ad4cb1484dd31cc0caa5fc845e
jkloos <jkloos@mpi-sws.org>
2016-12-22 16:38:04 +0100

    Documentation

--- cleanLog.mli
+++ cleanLog.mli
@@ -1,15 +1,48 @@
+(** {1 EventRacer event logs} *)
+(** Event logs capture certain events during the execution of a website.
+  * The instrumented browsers provided by the event racer project
+  * create event logs that contain information about reads from and write
+  * to memory, task creation and task ordering, as well as scoping information.
+  *)
+
+(** {2 The happens-before graph.} *)
+(** The vertices are labelled with event numbers, while the edges are
+  * labelled with [Some t] for timed events with a delay of [t], or
+  * [None] for other events/happens-before conditions. *)
 module DependencyGraph :
   Graph.Sig.P with type V.t = int with type E.t = int * int option * int
+
+(** {2 The event structure.} *)
+(** Commands that can occur in an event. *)
 type command =
+    (** [Read loc data]: Read from location [loc], yielding [data]. *)
     Read of string option * string option
+    (** [Write loc data]: Write [data] to location [loc]. *)
   | Write of string option * string option
+    (** [Post id]: Post task [id] *)
   | Post of int
+    (** [Enter scope]: Enter scope [scope] *)
   | Enter of string option
+    (** Exit scope *)
   | Exit
+
+(** A single event. *)
 type event = {
+  (** Type of the event. *)
   evtype : EventRacer.event_action_type;
+  (** Number of the event. *)
   id : int;
+  (** Commands executed by the event. *)
   commands : command list;
 }
-type trace = { events : event list; deps : DependencyGraph.t; }
+
+(** A trace. *)
+type trace = {
+  (** List of events. *)
+  events : event list;
+  (** Happens-before graph. *)
+  deps : DependencyGraph.t;
+}
+
+(** [load filename]: Load a trace from [filename]. *)
 val load : string -> trace
--- eventRacer.mli
+++ eventRacer.mli
@@ -1,18 +1,35 @@
+(** {1 Access to raw EventRacer log files} *)
+(** This module allows access to the raw contents of EventRacer log files,
+  * decoded only to the point where the enumerations have been resolved,
+  * strings instantiated and basic data structures turned to their OCaml
+  * equivalent. *)
+
+(** Command types, used in event actions. *)
 type command =
-    Enter_scope of string option
-  | Exit_scope
-  | Read_memory of string option
-  | Write_memory of string option
-  | Post of int
-  | Value of string option
+    Enter_scope of string option (* Entering a scope; argument: scope name *)
+  | Exit_scope (* Exiting a scope *)
+  | Read_memory of string option (* Read a value; argument: reference *)
+  | Write_memory of string option (* Write a value; argument: reference *)
+  | Post of int (* Post a task; argument: task identifier *)
+  | Value of string option (* Value used in a memory access; argument: value *)
+
+(** Type of an event action. *)
 type event_action_type =
     Unknown
   | Timer
   | UserInterface
   | Network
   | Continuation
+
+(** Happens-before edge in the hb graph. *)
 type arc = { tail : int; head : int; duration : int; }
+
+(** An event action, consisting of type and executed commands. *)
 type event_action = { evtype : event_action_type; commands : command array; }
+
+(** An event log, consisting of events and hb graph edges .*)
 type event_log = { events : event_action array; arcs : arc array; }
 
+(** [read_event_log filename] reads an event log from the given
+  * file. *)
 val read_event_log : string -> event_log
