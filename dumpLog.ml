5ddd74b110203ab46be926768f4b3b7de6da1173
jkloos <jkloos@mpi-sws.org>
2017-02-06 16:56:44 +0100

    Output races in dumplog.

--- dumpLog.ml
+++ dumpLog.ml
@@ -36,10 +36,26 @@ let pp_event_action pp (i, { evtype; commands }) =
     pp_event_action_type evtype
     (array ~sep:sp pp_command) commands
 
-let pp_event_log pp { events; arcs } =
-  pf pp "@[<v>Events:@ %a@ @ @[<v2>Arcs:@ %a@ @]@]"
+let str_access = function
+  | Read -> "read"
+  | Write -> "write"
+  | Update -> "update"
+
+let pp_access = using str_access string
+
+let pp_race pp
+      { ri_access1; ri_access2; ri_event1; ri_event2; ri_cmd1; ri_cmd2;
+        ri_var; ri_covered } =
+  pf pp "@[<hov>%a@%d:%d -@ %a@%d:%d@ on %d,@ parent: %d@]"
+    pp_access ri_access1 ri_event1 ri_cmd1
+    pp_access ri_access2 ri_event2 ri_cmd2
+    ri_var ri_covered
+
+let pp_event_log pp { events; arcs; races } =
+  pf pp "@[<v>Events:@ %a@ @ @[<v2>Arcs:@ %a@ @]@ @ @[<v2>Races:@,%a@ ]@]"
     (iter_bindings ~sep:sp Array.iteri pp_event_action) events
     (array ~sep:sp pp_arc) arcs
+    (array ~sep:cut pp_race) races
 
 let dump_one filename =
   Format.printf "@[<v>Log file %s:@ %a@ @ @]"
